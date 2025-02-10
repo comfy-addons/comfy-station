6735869f440d9561d74af5a816ec381cef730b52
Công Tiến Trần <tctien342@gmail.com>
2025-02-10 10:44:02 +0700

    feat(TimeAgoSpan): update each 5 seccond

--- components/TimeAgo.tsx
+++ components/TimeAgo.tsx
@@ -3,6 +3,7 @@ import en from 'javascript-time-ago/locale/en'
 import vi from 'javascript-time-ago/locale/vi'
 import zh from 'javascript-time-ago/locale/zh'
 import { useLocale } from 'next-intl'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 TimeAgo.addLocale(en)
 TimeAgo.addLocale(vi)
@@ -10,16 +11,28 @@ TimeAgo.addLocale(zh)
 // Initialize TimeAgo with the current locale
 const getTimeAgo = (locale: string) => new TimeAgo(locale)
 
+const REFETCH_INTERVAL = 5000
+
 export const TimeAgoSpan: IComponent<{
   date: Date
   prefix?: string
   className?: string
 }> = ({ prefix, date, className }) => {
+  const [timeStr, setTimeStr] = useState(date.toLocaleString())
   const locale = useLocale()
-  const timeAgo = getTimeAgo(locale)
+  const timeAgoRef = useRef(getTimeAgo(locale))
+
+  const updateTime = useCallback(() => setTimeStr(timeAgoRef.current.format(date, 'twitter-now')), [date, timeAgoRef])
+
+  useEffect(() => {
+    updateTime()
+    const interval = setInterval(updateTime, REFETCH_INTERVAL)
+    return () => clearInterval(interval)
+  }, [updateTime])
+
   return (
     <span className={className}>
-      {prefix} {timeAgo.format(date)}
+      {prefix} {timeStr}
     </span>
   )
 }
